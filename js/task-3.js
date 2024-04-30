7082c727e8ad3f241840d39b5af534db64c41651
Viktoria Zharina <159477668+ViktoriaZharina@users.noreply.github.com>
2024-04-30 16:51:10 +0300

    allDone

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,21 @@
 `use strict`;
 function checkForSpam(message) {
+    let spamCheck = "spam";
+    let saleCheck = "sale";
+    let spamLower = spamCheck.toLowerCase();
+    let saleLower = saleCheck.toLowerCase();
+    let checkWord = message.toLowerCase();
     
+    if (checkWord.includes(spamLower) || checkWord.includes(saleLower))
+        return true;
+    else
+        return false;
 }
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
